15d428313ed4a2ed88a560df38670ea59c036456
James Atong <havocjamo@gmail.com>
2026-06-17 17:08:53 +0300

    Remove article app

--- article/urls.py
+++
@@ -1,6 +0,0 @@
-from django.views.generic import TemplateView
-from django.urls import path
-
-urlpatterns = [
-    path('', TemplateView.as_view(template_name="article/index.html"), name='index'),
-]
